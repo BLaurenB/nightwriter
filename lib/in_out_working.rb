def0b524e527cb946fceea20e6962506c1cdde5a
BLaurenB <blbillington1@gmail.com>
2017-08-27 08:49:35 -0600

    updating master branch

--- lib/in_out_working.rb
+++ lib/in_out_working.rb
@@ -1,3 +1,6 @@
+
+
+
 require 'pry'
 
 # my_array = IO.readlines('filename.txt')
@@ -7,33 +10,32 @@ class InOut
   def read_in_file
     @file_chars_array = IO.readlines("./lib/test_text_file.txt")
     # this returns 2 (2 lines that are separated into 2 array elements)
+
   end
 
 
-  # def count_chars_in_read_file
-  #   @count_chars_in_file = @file_chars_array
-  #     @count_chars_in_file[0].split("").length
-  # end
-
   def write_to_file #this works
     File.open("./lib/Output.txt", "w") do |file|
-      file.write(@file_chars_array << "This is a new line of text.") #this also works by sending an array to teh text file_chars_array
+      file.write(@file_chars_array << "This is a new line of text.") #this also works by sending an array of 3 elements (3 lines) to the text file_chars_array
 
     end
   end
 
   def count_output_chars
     @count_output_chars = IO.readlines("./lib/Output.txt")
-    @count_output_chars.flatten
-    @count_output_chars[0].split("").length
+    @count_output_chars = @count_output_chars.flatten
+    @count_output_chars = @count_output_chars[0].split("").length
+    #this takes the array made up of 1 line of text per element, flattens the elements into 1, splits it up to count it, and converts it to a string so it can be interpolated.
+    "Created 'Output.txt' containing  #{@count_output_chars}  characters"
+
   end
 
-  puts "Created 'Output.txt' containing #{@count_output_chars} chars"
+
 
 
 end
 
-inout = InOut.new
-# binding.pry
-inout.read_in_file
-inout.write_to_file
+# inout = InOut.new
+# # binding.pry
+# inout.read_in_file
+# inout.write_to_file
