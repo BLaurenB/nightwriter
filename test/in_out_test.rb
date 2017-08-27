4be3ddc8168e7465c023ad96d2643e010ce04066
BLaurenB <blbillington1@gmail.com>
2017-08-27 12:30:40 -0600

    updating so that I can pull Dione's

--- test/in_out_test.rb
+++ test/in_out_test.rb
@@ -1,13 +1,20 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/night_writer'
+require './lib/in_out.rb'
 
 class NightWriterTest < Minitest::Test
 
-  def test_it_exists
+  def test_InOut_exists
     inout = InOut.new
-    assert_instance_of inout
-  end 
+    assert_instance_of InOut, inout
+  end
+
+  def it_it_can_read_in_files #Not sure how to reallt test this!
+    inout= InOut.new
+    ! assert_nil inout.read_in_file
+  end
+
+
 
 end
