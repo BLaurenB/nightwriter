08e262cb45f77497c6bae1993f4646490fb00859
Dione Wilson <dionew1@gmail.com>
2017-08-27 12:30:55 -0600

    added comments for clarity

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -53,12 +53,15 @@ class NightWriter
     @bottom = []
   end
 
+=begin
   def print_letter(letter)
     #input one character
     @characters[letter].join "\n"
     #takes the letter key and returns the value array
     #joins with new line character
   end
+  (this method may no longer be needed)
+=end
 
   def print_character(letter)
     #require 'pry'; binding.pry
@@ -75,11 +78,15 @@ class NightWriter
 
   def print_words(word)
     character = word.split("")
+    #splits word into characters
     character.each { |letter| print_character(letter)}
+    #should be sending one character at a time to print_character method
     line_1 = @top.join("")
     line_2 = @middle.join("")
     line_3 = @bottom.join("")
+    #creates one string from each array
     line_1 + "\n" + line_2 + "\n" + line_3
+    #joinging the strings with a new line before each array
   end
 
 end
